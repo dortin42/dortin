e3544a04c36a0f5ab740791817988dc8ef601964
nintF1link <jalagut8@gmail.com>
2019-02-15 22:00:41 -0500

    Updates

---
+++ precache-manifest.505756857be2fb42500b33065697c76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "374096f005fe3277bb6f",
+    "url": "/static/js/main.374096f0.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "374096f005fe3277bb6f",
+    "url": "/static/css/main.4fc4437c.chunk.css"
+  },
+  {
+    "revision": "73844d8e50489f54e7d170f26cd3c96c",
+    "url": "/index.html"
+  }
+];
